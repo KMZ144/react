d2509b9ec51d8be2795456a20c2ba1c015d62137
Kareem_Mostafa <kareemmostafa114@gmail.com>
2023-03-30 14:14:49 +0200

    search edited

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -13,9 +13,9 @@ export default function Product({product}) {
         <Card className='col-sm-6 col-md-3 mt-4 text-center ms-3'>
         <Card.Img variant="top" src={product.imgUrl} className='product' />
         <Card.Body>
-          <Card.Title>{product.title}</Card.Title>
+          <Card.Title>title: {product.title}</Card.Title>
           <Card.Text>
-            {product.price}
+           price: {product.price}
           </Card.Text>
           <Button variant="primary" className='ms-2' onClick={()=>{showDetails(product.id)}}>Show Details</Button>
         </Card.Body>
--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,51 +1,52 @@
-import React, { useState } from 'react'
-import Articles from './Articles';
+import React, { useState } from "react";
+import Articles from "./Articles";
 
 export default function Search() {
-    let data = [
-        {
-          id: 1,
-          title: "sdsdsd",
-          desc: "sdsdsd",
-        },
-        {
-            id:2,
-            title: "gggg",
-            desc: "gggg",
-          },
-          {
-            id: 3,
-            title: "hhhh",
-            desc: "hhhh",
-          },
-          {
-            id: 4,
-            title: "jjjj",
-            desc: "jjjj",
-          },
-      ];
-      const [articles,setArticles]=useState(data)
-      let [flag,setFlag]=useState(true)
-    const search=(e)=>{
-       let query=e.target.value
-       setArticles(articles.filter((ele)=>ele.desc.match(query)))
-       setFlag(true)
-       if (query===''){
-        setArticles(data)
-       }
-       if(articles.length===0){
-     setFlag (false)
-       }
+  let data = [
+    {
+      id: 1,
+      title: "sdsdsd",
+      desc: "sdsdsd",
+    },
+    {
+      id: 2,
+      title: "gggg",
+      desc: "gggg",
+    },
+    {
+      id: 3,
+      title: "hhhh",
+      desc: "hhhh",
+    },
+    {
+      id: 4,
+      title: "jjjj",
+      desc: "jjjj",
+    },
+  ];
+  const [articles, setArticles] = useState(data);
+  const search = (e) => {
+    let query = e.target.value;
+    setArticles(articles.filter((ele) => ele.desc.match(query)));
+    if (query === "") {
+      setArticles(data);
     }
+  };
   return (
-    <div className='container w-50'>
-        <div className="mb-3">
-  <label  className="form-label">Search</label>
-  <input  className="form-control" placeholder="Search" onChange={search}/>
-</div>
-{
- flag ? <Articles articles={articles} />: <div>no matching </div>
-}
+    <div className="container w-50">
+      <div className="mb-3">
+        <label className="form-label">Search</label>
+        <input
+          className="form-control"
+          placeholder="Search"
+          onChange={search}
+        />
+      </div>
+      {articles.length !== 0 ? (
+        <Articles articles={articles} />
+      ) : (
+        <div>no matching </div>
+      )}
     </div>
-  )
+  );
 }
